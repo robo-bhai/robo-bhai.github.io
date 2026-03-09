458581a625f223b1d3444b478b7b38fcde9df2e5
robo-bhai <m.noman.bh2@gmail.com>
2026-03-09 09:28:52 +0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'hadi88-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './a.jpg',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
